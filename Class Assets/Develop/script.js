89e336eb176edd9257ad4d753dd6794625d6d8bb
jdavis3333 <jdavis3333@gmail.com>
2020-06-24 21:17:21 -0700

    dynamic time added to .js and functional

--- Class Assets/Develop/script.js
+++ Class Assets/Develop/script.js
@@ -1,7 +1,23 @@
 
 
-//Static timer
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+//Static time (day)
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+
+//Dynamic time
+let updateTime = function () {
+    let currentTime = moment().format('h:mm:ss')
+    $("#time").text(currentTime)
+    console.log(currentTime)
+}
+updateTime();
+
+//Dynamically update blocks based on time
+
+
+
+
+//Save timeblock text to local storage
 
 
 

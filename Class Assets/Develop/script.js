64bcab316298add2498fb0819b0c4e417e739d01
jdavis3333 <jdavis3333@gmail.com>
2020-06-24 19:24:39 -0700

    created script.js and deployed date&time to Jumbotron

---
+++ Class Assets/Develop/script.js
@@ -0,0 +1,27 @@
+
+
+//Static timer
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+
+
+
+
+
+
+
+
+
+    // // To initally set the times, we will call the functions
+    // updateTime();
+    // updateHour();
+    // updateMinutes();
+    // updateSeconds();
+    // countdown();
+
+    // // To continuously call the functions, we will use setInterval
+    // setInterval(updateTime, 1000);
+    // setInterval(updateHour, 1000);
+    // setInterval(updateMinutes, 1000);
+    // setInterval(updateSeconds, 1000);
+    // setInterval(countdown, 1);

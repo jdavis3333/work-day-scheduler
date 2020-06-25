1e1faaefbb367d03df9cb2303082a84bd52aaeca
jdavis3333 <jdavis3333@gmail.com>
2020-06-24 20:05:59 -0700

    completed time block elements

--- Class Assets/Develop/script.js
+++ Class Assets/Develop/script.js
@@ -11,17 +11,3 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
 
 
-
-    // // To initally set the times, we will call the functions
-    // updateTime();
-    // updateHour();
-    // updateMinutes();
-    // updateSeconds();
-    // countdown();
-
-    // // To continuously call the functions, we will use setInterval
-    // setInterval(updateTime, 1000);
-    // setInterval(updateHour, 1000);
-    // setInterval(updateMinutes, 1000);
-    // setInterval(updateSeconds, 1000);
-    // setInterval(countdown, 1);
